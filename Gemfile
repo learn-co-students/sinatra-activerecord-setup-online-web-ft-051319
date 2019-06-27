9f7d7bc65d4926ba40ec5da2661db43d10f4b1a6
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-06-28 03:27:01 +0800

    Done.

--- Gemfile
+++ Gemfile
@@ -4,9 +4,14 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'thin'
 gem 'require_all'
+gem 'activerecord', '5.2'
+gem 'sinatra-activerecord'
+gem 'rake'
 
 
-group :development do
+group :development do #none of these are required in production, theyre just useful for developers
 	gem 'shotgun'
 	gem 'pry'
+	gem 'tux'
+	gem 'sqlite3', '~> 1.3.6'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
-ENV['SINATRA_ENV'] ||= "development"
+ENV['SINATRA_ENV'] ||= "development" #tells Sinatra that it should use the "development" environment for both shotgun and the testing suite
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/database.db' #this creates a folder called db and place inside it the file "database.db" but only after we rake
+end
 
 require './app'
